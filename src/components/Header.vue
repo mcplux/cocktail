95414883768d93a2fbaee1f4bbf25d30cb3d46af
Juan Martinez <pablomar01@outlook.com>
2024-01-12 17:46:10 -0500

    nav created

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,32 @@
 <script setup>
+import { computed } from 'vue'
+import { RouterLink, useRoute } from 'vue-router'
 
+const route = useRoute()
+
+const homePage = computed(() => route.name === 'home')
 </script>
 
 <template>
-  <header>
+  <header class="bg-slate-800" :class="[homePage ? `bg-[url('/img/bg.jpg')] bg-cover bg-center` : '']">
     <div class="mx-auto container px-5 py-16">
-      <div class=""></div>
+      <div class="flex justify-between items-center">
+        <div>
+          <RouterLink :to="{name: 'home'}">
+            <img class="w-32" src="/img/logo.svg" alt="Logo">
+          </RouterLink>
+        </div>
+        <nav class="flex gap-4">
+          <RouterLink :to="{name: 'home'}" class="text-white uppercase font-bold" active-class="text-orange-500">
+            Home
+          </RouterLink>
+          <RouterLink :to="{name: 'favorites'}" class="text-white uppercase font-bold" active-class="text-orange-500">
+            Favorites
+          </RouterLink>
+        </nav>
+      </div>
 
-      <form action="" class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+      <form action="" class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6" v-if="homePage">
         <div class="space-y-4">
           <label for="ingredient" class="block text-white uppercase font-extrabold text-lg">
             Name or Ingredients
---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="text-6xl font-extrabold">
+    Favorites
+  </h1>
+</template>
